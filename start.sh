34de7906411b5ae92da38ba66ccc2e94dfec1ea4
Regaus <mr.regaus2@gmail.com>
2022-01-19 20:00:17 +0000

    Put the code to update Regaus.py into a separate file

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 git pull
-# Note to self: When I update the module, just run that command by myself
-# python3.9 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
 nohup python3.9 -u index.py &>data/log.out &
 tail -f data/log.out
---
+++ start_rpy.sh
@@ -0,0 +1,4 @@
+git pull
+python3.9 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
+nohup python3.9 -u index.py &>data/log.out &
+tail -f data/log.out
