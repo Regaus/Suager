2a309e5ab1348ec3195d0f460a5baa08d8a3bef9
Regaus <mr.regaus2@gmail.com>
2022-08-12 23:46:06 +0100

    Change update scripts to using `python` without specifying version

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 git pull
-nohup python3.9 -u index.py &>data/log.out &
+nohup python3 -u index.py &>data/log.out &
 tail -f data/log.out
--- start_rpy.sh
+++ start_rpy.sh
@@ -1,4 +1,4 @@
 git pull
-python3.9 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
-nohup python3.9 -u index.py &>data/log.out &
+python3 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
+nohup python3 -u index.py &>data/log.out &
 tail -f data/log.out
