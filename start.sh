2eb882e218818113039a1a016acd5c3b832483a5
Regaus <mr.regaus2@gmail.com>
2022-08-13 02:43:41 +0100

    Fix linux being a retarded piece of shit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 git pull
-nohup python3 -u index.py > data/log.out 2>&1 </dev/null &
+nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
--- start_rpy.sh
+++ start_rpy.sh
@@ -1,4 +1,4 @@
 git pull
 python3 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
-nohup python3 -u index.py &>data/log.out &
+nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
