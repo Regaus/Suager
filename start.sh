d1eebf91ba66e08accc6718275535b4bd0ba96ff
Regaus <mr.regaus2@gmail.com>
2022-09-11 01:51:50 +0100

    Try making it possible to update the system while restarting the bot

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt upgrade -y
 git pull
 nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
--- start_rpy.sh
+++ start_rpy.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt upgrade -y
 git pull
 python3 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
 nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
