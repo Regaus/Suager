c07c43319aabbd4221c3cc458201a5c78be7cc30
Regaus <mr.regaus2@gmail.com>
2022-09-11 01:55:25 +0100

    Make the startup script look cleaner in console by visually separating each section

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
+echo ""
 git pull
+echo ""
 nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
--- start_rpy.sh
+++ start_rpy.sh
@@ -1,6 +1,9 @@
 sudo apt update
 sudo apt upgrade -y
+echo ""
 git pull
+echo ""
 python3 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
+echo ""
 nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
