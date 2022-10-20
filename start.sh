7c743e39eafa3d666bca49194c94ac78602ddce3
Regaus <mr.regaus2@gmail.com>
2022-10-20 21:43:23 +0100

    Specifically call python 3.11 in startup files

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ sudo apt upgrade -y
 echo ""
 git pull
 echo ""
-nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
+nohup python3.11 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
--- start_rpy.sh
+++ start_rpy.sh
@@ -5,5 +5,5 @@ git pull
 echo ""
 python3 -m pip install -U "git+https://github.com/Regaus/Regaus.py.git@stable#egg=regaus.py"
 echo ""
-nohup python3 -u index.py > data/log.out 2>&1 < /dev/null &
+nohup python3.11 -u index.py > data/log.out 2>&1 < /dev/null &
 tail -f data/log.out
