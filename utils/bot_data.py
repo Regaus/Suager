be5ffe5d6f7d84d7a2defa2eb0fb8b5ba0d70a9b
Regaus <mr.regaus2@gmail.com>
2022-12-20 14:20:56 +0000

    Reduced frequency of origin surnames

--- utils/bot_data.py
+++ utils/bot_data.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from utils import commands, languages, permissions
-from utils.database import Database
 
 # List of all cogs each bot needs to load
 load = {
@@ -64,7 +63,7 @@ load = {
 
 
 class Bot(commands.AutoShardedBot):
-    def __init__(self, blacklist: list, index: int, lc: dict, config: dict, name: str, db: Database, *args, **kwargs):
+    def __init__(self, blacklist: list, index: int, lc: dict, config: dict, name: str, db, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.blacklist = blacklist
         self.index = index
